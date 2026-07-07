107240230e96be87ada11891873f8765c48cf2fc
PyTs1n9 <1990629881@qq.com>
2026-07-08 00:52:48 +0800

    commit 03 两数相加

--- PyTs1N9/LeetCode/0002 两数相加/0002 两数相加.py
+++ PyTs1N9/LeetCode/0002 两数相加/0002 两数相加.py
@@ -1,4 +1,41 @@
-# 列表思维 👇👇👇
+################# LeetCode环境已经打好 ###############
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+####################################################
+class Solution:
+
+    def addTwoNumbers(
+        self,
+        l1: Optional[ListNode],
+        l2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+
+        dummy = ListNode(0)
+        cur = dummy
+        carry = 0
+
+        while l1 or l2 or carry:
+            x = l1.val if l1 else 0
+            y = l2.val if l2 else 0
+
+            total = x + y + carry
+            carry = total // 10
+            cur.next = ListNode(total % 10)
+
+            cur = cur.next
+
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+
+        return dummy.next
+
+# 列表思维 👇👇👇链表不支持下标或切片
 # def addTwoNumbers(l1,l2):
 # 	num1 = int(''.join(map(str, l1[::-1])))
 # 	num2 = int(''.join(map(str, l2[::-1])))
