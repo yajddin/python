5ac5f6eaf48cb493992e223229cafed4b0d8a9db
PyTs1n9 <1990629881@qq.com>
2026-07-09 20:07:19 +0800

    commit 06 打折购买糖果的最小开销

--- PyTs1N9/LeetCode/2144 打折购买糖果的最小开销/2144 打折购买糖果的最小开销.py
+++ PyTs1N9/LeetCode/2144 打折购买糖果的最小开销/2144 打折购买糖果的最小开销.py
@@ -1,3 +1,11 @@
+# C语言运行消耗内存9MB Python语言运行消耗内存19MB 消耗内存差异原因如下：
+# C 的 int 数组：
+#  1. 一块连续内存，直接存整数
+#  2. 原地排序，额外内存很少
+# Python 的 list：
+#  1. 一块指针数组，每个指针再指向一个 Python int 对象，存的是对象
+#  2. 带解释器运行
+
 def minimumCost(cost) -> int:
 	cost.sort()
 	cost.reverse()
