68d9fe3d33ab7610d40e296257ed0e4e5b2fa6b2
PyTs1n9 <1990629881@qq.com>
2026-07-09 17:31:41 +0800

    commit 03 打折购买糖果的最小开销

--- PyTs1N9/LeetCode/2144 打折购买糖果的最小开销/2144 打折购买糖果的最小开销.py
+++ PyTs1N9/LeetCode/2144 打折购买糖果的最小开销/2144 打折购买糖果的最小开销.py
@@ -1,12 +1,19 @@
 cost = [6, 5, 7, 9, 2, 2]
 cost.sort()
 cost.reverse()
+
 sums = 0
 length = len(cost)
 
-for _ in range(2, length, 3):
-	print(_)
-	print(cost[_])
-	cost[_] = 0
-for _ in range(length):
-	sums += cost[_]
+# 优化后
+for i in range(length):
+    if i >= 2 and (i - 2) % 3 == 0:
+        cost[i] = 0
+    sums += cost[i]
+
+# for _ in range(2, length, 3):
+# 	print(_)
+# 	print(cost[_])
+# 	cost[_] = 0
+# for _ in range(length):
+# 	sums += cost[_]
