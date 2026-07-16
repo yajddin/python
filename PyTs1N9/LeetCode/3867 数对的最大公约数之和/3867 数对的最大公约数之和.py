c40c4710eb324819e06f91ac8ebb07f55661726f
PyTs1n9 <1990629881@qq.com>
2026-07-16 10:49:14 +0800

    commit 7.16 数对的最大公约数之和

--- PyTs1N9/LeetCode/3867 数对的最大公约数之和/3867 数对的最大公约数之和.py
+++ PyTs1N9/LeetCode/3867 数对的最大公约数之和/3867 数对的最大公约数之和.py
@@ -1,13 +1,27 @@
 from math import gcd
 
+# 改为保存当前最大值，复杂度降为 O(n log n)：
 nums = [3, 6, 2, 8]
 prefixGcd = []
-for i in range(len(nums)):
-	max_num = max(nums[:i + 1])
-	prefixGcd.append(gcd(nums[i], max_num))
+max_num = 0
+for num in nums:
+    max_num = max(max_num, num)
+    prefixGcd.append(gcd(num, max_num))
 prefixGcd.sort()
-pairs = [
-	(prefixGcd[i], prefixGcd[-i - 1])
-	for i in range(len(prefixGcd) // 2)
-]
-result = sum(gcd(a, b) for a, b in pairs)
+result = sum(
+    gcd(prefixGcd[i], prefixGcd[-i - 1])
+    for i in range(len(prefixGcd) // 2)
+)
+
+# 超出时间限制 双重循环计算前缀最大值，时间复杂度是 O(n²)
+# nums = [3, 6, 2, 8]
+# prefixGcd = []
+# for i in range(len(nums)):
+# 	max_num = max(nums[:i + 1])
+# 	prefixGcd.append(gcd(nums[i], max_num))
+# prefixGcd.sort()
+# pairs = [
+# 	(prefixGcd[i], prefixGcd[-i - 1])
+# 	for i in range(len(prefixGcd) // 2)
+# ]
+# result = sum(gcd(a, b) for a, b in pairs)
